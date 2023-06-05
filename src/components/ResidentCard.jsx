294b307cd5b3b67f2d0569594f9786241369eaf3
LuisEnrique25 <luisrg2225@gmail.com>
2023-06-04 22:37:26 -0600

    Agregue la animacion del bosShadow tipo neon al tailwind.config y se lo aplique a la card correspondiente, lo hice que coinsidiera con el status del personaje, la card2 me gusto mas asi que la dejare, FALTA ESTILO A LA LISTA DE PAGINACION Y A LOS BOTONES DEL MODO OSCURO

--- src/components/ResidentCard.jsx
+++ src/components/ResidentCard.jsx
@@ -10,9 +10,9 @@ const ResidentCard = ({residentURL}) => {
         "unknown" : "bg-gray-600"
     }
     const borderStatus = {
-        "Alive": "border-green-500",
-        "Dead": "border-red-500", 
-        "unknown" : "border-gray-600"
+        "Alive": "animate-neonalive",
+        "Dead": "animate-neondead", 
+        "unknown" : "animate-neonunknown"
     }
 
     useEffect(() => {
@@ -25,7 +25,7 @@ const ResidentCard = ({residentURL}) => {
     <article className='flex flex-col justify-center items-center'>
         
         {/** IMAGEN */}
-        <div className={`relative border-4 ${borderStatus[residentInfo?.status]}  h-fit w-fit    rounded-[50%] overflow-hidden z-50`}>
+        <div className={`relative  ${borderStatus[residentInfo?.status]}  h-fit w-fit    rounded-[50%] overflow-hidden z-50`}>
             <img src={residentInfo?.image} alt={residentInfo?.name} className='h-[200px] sm:h-[250px]'/>
 
             <div className={`flex items-center gap-2 absolute bottom-4 left-1/2 -translate-x-[50%] capitalize p-1 text-black bg-white/30 backdrop-blur-sm rounded `} >
@@ -36,17 +36,17 @@ const ResidentCard = ({residentURL}) => {
 
 
         {/** INFO */}
-        <section  className={`border-4 ${borderStatus[residentInfo?.status]} rounded-2xl bg-purple-950 dark:bg-purple-500 overflow-hidden min-w-[250px] text-black dark:text-white  -translate-y-10  `}>
+        <section  className={` ${borderStatus[residentInfo?.status]} rounded-2xl bg-emerald-300/20 dark:bg-purple-900/50 backdrop-blur-sm overflow-hidden min-w-[250px] text-black dark:text-white  -translate-y-5 `}>
             
             
-                <div className='bg-purple-500 dark:bg-purple-950'>
-                    <h4 className='text-xl font-semibold bg-orange-300 dark:bg-slate-900 text-center p-2 pt-8 rounded-b-lg border-none'>{residentInfo?.name}</h4>
+                <div className=''>
+                    <h4 className={` text-xl font-semibold text-center p-2 pt-8 rounded-b-lg border-none backdrop-blur-sm ${statusStyles[residentInfo?.status]} bg-opacity-20 `}>{residentInfo?.name}</h4>
                 </div>
 
-                <ul className='p-2 bg-purple-500 dark:bg-purple-950 rounded-br-[4rem] font-semibold'>
-                    <li className='p-3 flex justify-between'>Specie: <span className='text-emerald-900 dark:text-emerald-500'>{residentInfo?.species}</span> </li>
-                    <li className='p-3 flex justify-between' >Origin: <span className='text-emerald-900 dark:text-emerald-500'>{residentInfo?.origin.name}</span> </li>
-                    <li className='p-3 flex justify-between'>Times appear: <span className='text-emerald-900 dark:text-emerald-500'>{residentInfo?.episode.length} </span> </li>
+                <ul className='p-2 font-semibold'>
+                    <li className='p-3 flex justify-between'>Specie: <span className='text-emerald-900 dark:text-emerald-200'>{residentInfo?.species}</span> </li>
+                    <li className='p-3 flex justify-between' >Origin: <span className='text-emerald-900 dark:text-emerald-200'>{residentInfo?.origin.name}</span> </li>
+                    <li className='p-3 flex justify-between'>Times appear: <span className='text-emerald-900 dark:text-emerald-200'>{residentInfo?.episode.length} </span> </li>
                 </ul>
             
         </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,25 @@ export default {
   theme: {
     extend: {
       animation: {
-        'spin-slow': 'spin 8s linear infinite'
-      }
+        'spin-slow': 'spin 8s linear infinite',
+        'neonalive' : 'neonalive 1s linear infinite alternate',
+        'neondead' : 'neondead 1s linear infinite alternate',
+        'neonunknown' : 'neonunknown 1s linear infinite alternate'
+      },
+      keyframes:{
+        neonalive: {
+          '0%, 100%': { boxShadow : '0 0 5px rgb(34 197 94), 0 0 10px rgb(34 197 94), 0 0 20px rgb(109, 202, 143), 0 0 25px rgb(109, 202, 143) ' }, 
+          '50%': { boxShadow: '0 0 5px rgb(34 197 94), 0 0 10px rgb(34 197 94), 0 0 20px rgb(109, 202, 143), 0 0 25px rgb(109, 202, 143), 0 0 30px rgb(109, 202, 143) '  },
+        },
+        neondead: {
+          '0%, 100%': { boxShadow : '0 0 5px rgb(239 68 68), 0 0 10px rgb(239 68 68), 0 0 20px rgb(223, 129, 129), 0 0 25px rgb(223, 129, 129) ' }, 
+          '50%': { boxShadow: '0 0 5px rgb(239 68 68), 0 0 10px rgb(239 68 68), 0 0 20px rgb(223, 129, 129), 0 0 25px rgb(223, 129, 129), 0 0 30px rgb(223, 129, 129) '},
+        },
+        neonunknown: {
+          '0%, 100%': { boxShadow : '0 0 5px rgb(107 114 128), 0 0 10px rgb(107 114 128), 0 0 20px rgb(170, 171, 175), 0 0 25px rgb(170, 171, 175) ' }, 
+          '50%': { boxShadow: '0 0 5px rgb(107 114 128), 0 0 10px rgb(107 114 128), 0 0 20px rgb(170, 171, 175), 0 0 25px rgb(170, 171, 175), 0 0 30px rgb(170, 171, 175)'},
+        }
+      },
     },
   },
   plugins: [],
